aa47bee671a87279d50142191ed94235dda021c5
Michael Martinez <zeroeclipse@live.com>
2021-05-22 21:55:38 -0400

    commit tree 0e025a3cf186f74055a1539b3de3c60bbb3b2445

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    let array = []
+    array.push(drivers[0])
+    array.push(drivers[1])
+    return array
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    let array = []
+    array.push(drivers[drivers.length-2])
+    array.push(drivers[drivers.length-1])
+    
+    return array
+}
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(int) {
+    return function(fare) {
+        return fare * int
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(array, func) {
+    return func(array)
+}
